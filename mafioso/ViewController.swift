328daa3d51454f08c8fd7c440bcd066b717fcddb
Wameedh Mohammed Ali <wameedh.wf@gmail.com>
2018-09-27 19:43:10 -0700

    Initial Commit

---
+++ mafioso/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mafioso
+//
+//  Created by Wameedh Mohammed Ali on 9/27/18.
+//  Copyright © 2018 Wameedh Mohammed Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
